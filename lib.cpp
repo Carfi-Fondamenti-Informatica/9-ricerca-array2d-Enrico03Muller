5e84f936437662b7a867c4e57acd785291009006
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-12-11 16:51:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+//
+// Created by Enrico Muller on 11/12/2021.
+//
 
+#include "lib.h"
+#include <cstring>
+
+bool controllo (char lista[4][20], char nome[1][20], int &x){
+    for (int i = 0; i < 4; i++) {
+        if (strcmp (lista [i], nome [0]) == 0) {
+            x = i;
+            return true;
+        }
+    }
+    return false;
+}
