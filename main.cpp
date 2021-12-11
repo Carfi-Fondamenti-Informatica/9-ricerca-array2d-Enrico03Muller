ff58ae38cfb2030d9b6a2e5ddc580a4b9ff81368
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-12-11 16:51:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char lista [4][20];
+    char nome [1][20];
+    int pos = 0;
+
+    //Ingresso lista nomi
+    for (int i = 0; i < 4; i++){
+        cin >> lista [i];
+    }
+
+    //Ingresso nome da cercare
+    cin >> nome [0];
+
+    if (controllo(lista, nome, pos)){
+        cout << pos;
+    }else{
+        cout << "non presente";
+    }
+    return 0;
 }
