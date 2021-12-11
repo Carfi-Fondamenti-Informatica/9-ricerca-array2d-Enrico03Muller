75377a499e1e2c33d180d1846e0772d2aef30745
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-12-11 16:51:28 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+//
+// Created by Enrico Muller on 11/12/2021.
+//
 
+#ifndef ESERCIZIO9_0_LIB_H
+#define ESERCIZIO9_0_LIB_H
+bool controllo (char lista[4][20], char nome[1][20], int &x);
+#endif //ESERCIZIO9_0_LIB_H
